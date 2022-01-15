634ed9442b6ceac8ccae52994399c4349687fe6b
Kevin Darlington <kevin@outroot.com>
2022-01-14 22:46:04 -0500

    chore: remove commented out code

--- src/main.rs
+++ src/main.rs
@@ -13,73 +13,27 @@ use windows::Win32::{
 };
 
 static KEYBOARD_HOOK: OnceCell<HHOOK> = OnceCell::new();
-// static DISABLE_WIN: OnceCell<AtomicBool> = OnceCell::new();
 
 extern "system" fn keyboard_hook(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
   let ev = unsafe { *(lparam.0 as *const KBDLLHOOKSTRUCT) };
 
-  //   let scancode = ev.scanCode;
-
   match (ev.vkCode as VIRTUAL_KEY, wparam.0 as u32) {
     (VK_LWIN, WM_KEYDOWN | WM_SYSKEYDOWN | WM_KEYUP | WM_SYSKEYUP) => {
-      // if DISABLE_WIN.get().unwrap().load(Ordering::Relaxed) {
       let state = unsafe { SHQueryUserNotificationState().unwrap() };
       if state == QUNS_BUSY || state == QUNS_RUNNING_D3D_FULL_SCREEN {
         return LRESULT(1);
       }
     },
-    _ => {
-      //   println!("{:?}", ev);
-    },
+    _ => {},
   };
 
   unsafe { CallNextHookEx(KEYBOARD_HOOK.get().unwrap(), code, wparam, lparam) }
 }
 
-// unsafe extern "system" fn foreground_hook(
-//   _hwineventhook: HWINEVENTHOOK,
-//   _event: u32,
-//   hwnd: HWND,
-//   _idobject: i32,
-//   _idchild: i32,
-//   _ideventthread: u32,
-//   _dwmseventtime: u32,
-// ) {
-//   //   let mut text: [u16; 512] = [0; 512];
-//   //   let len = GetClassNameW(hwnd, PWSTR(text.as_mut_ptr()), text.len() as i32);
-//   //   let text = String::from_utf16_lossy(&text[..len as usize]);
-//   //   println!("{:?}", text);
-
-//   let state = SHQueryUserNotificationState().unwrap();
-//   println!("{:?}", state);
-//   if state == QUNS_BUSY || state == QUNS_RUNNING_D3D_FULL_SCREEN {
-//     DISABLE_WIN.get().unwrap().store(true, Ordering::Relaxed);
-//   } else {
-//     DISABLE_WIN.get().unwrap().store(false, Ordering::Relaxed);
-//   }
-// }
-
 fn main() {
-  // let fg_win = unsafe { GetForegroundWindow() };
-  // println!("{:?}", fg_win);
-
-  // DISABLE_WIN.set(AtomicBool::new(false)).unwrap();
-
   let hhk =
     unsafe { SetWindowsHookExW(WH_KEYBOARD_LL, Some(keyboard_hook), HINSTANCE::default(), 0) };
   KEYBOARD_HOOK.set(hhk).unwrap();
-
-  // unsafe {
-  //   SetWinEventHook(
-  //     EVENT_SYSTEM_FOREGROUND,
-  //     EVENT_SYSTEM_FOREGROUND,
-  //     HINSTANCE::default(),
-  //     Some(foreground_hook),
-  //     0,
-  //     0,
-  //     WINEVENT_OUTOFCONTEXT,
-  //   );
-  // }
 
   let mut message = MSG::default();
   unsafe {
